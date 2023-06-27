40c8c8cd8b411ef640d8e6ef87f9f46fa9eaf040
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2023-06-27 10:47:07 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,15 @@
-sudo apt-get update
-sudo apt-get install ffmpeg libsm6 libxext6 libzbar0 -y
-pip install -r requirements.txt
-
 WORK_DIR="/home/onyxia/work"
-CLONE_DIR="${WORK_DIR}/funathon-sujet4"
 
-# Clone course repository
-REPO_URL="https://github.com/InseeFrLab/funathon2023_sujet4.git"
-git clone --depth 1 $REPO_URL $CLONE_DIR
+# Clone project repository
+git clone --depth 1 https://github.com/InseeFrLab/funathon2023_sujet4.git
+chown -R onyxia:users "${WORK_DIR}/funathon2023_sujet4"
+
+# Install system libraries
+apt-get update && apt-get install ffmpeg libsm6 libxext6 libzbar0 -y
+
+# Install Python dependencies
+pip install -r "${WORK_DIR}/funathon2023_sujet4/requirements.txt"
 
 # Open the relevant notebook when starting Jupyter Lab
 jupyter server --generate-config
-echo "c.LabApp.default_url = '/lab/tree/funathon-sujet4/index.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
+echo "c.LabApp.default_url = '/lab/tree/work/funathon2023_sujet4/index.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
